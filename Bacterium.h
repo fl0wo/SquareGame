ccfc0d079953a28abd778594c5267f3a09fbd2fb
Florian Sabani <florian.sabani@generali.com>
2021-10-28 01:35:01 +0200

    Now using transparent white for enemy visibility radius.

--- Bacterium.h
+++ Bacterium.h
@@ -20,14 +20,15 @@ public:
         CircleShape circle = CircleShape(1.0F/2.0F*pixePerUnit);
         circle.setFillColor(Color::Yellow);
         circle.setPosition((this->getRealCol()*pixePerUnit)-cam.getX(),(this->getRealRow()*pixePerUnit)-cam.getY());
+        circle.setPointCount(100);
         //circle.move(-circle.getRadius() ,-circle.getRadius());
 
         //circle.setPosition(this->row,this->col);
         CircleShape ray2 = circle;
         ray2.setFillColor(Color::Transparent);
         ray2.setRadius(pixePerUnit * this->getVisibilityRay());
-        ray2.setOutlineColor(Color::White);
-        ray2.setOutlineThickness(2.5);
+        ray2.setOutlineColor(Color(255,255,255,50));
+        ray2.setOutlineThickness(1);
         ray2.move(-ray2.getRadius() ,-ray2.getRadius());
 
         vector<pair<int,int>> folPath = this->getFollowingPath();
